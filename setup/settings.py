601d719770d3cefce6ad01e98625bebac31f0bf8
Felipe Oliveira <felipeoes@usp.br>
2021-11-24 21:06:46 -0300

    initial files and first model

--- setup/settings.py
+++ setup/settings.py
@@ -17,12 +17,10 @@ from configobj import ConfigObj
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DJANGO_CONF_FILE = f'{BASE_DIR}/django.conf'
+DJANGO_CONFIG = ConfigObj(DJANGO_CONF_FILE)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(#(@$+2kzl5sn+d&w9gx+1)^-54jwh$b6(v&e)ffd2&st1duf@'
+SECRET_KEY = DJANGO_CONFIG['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
