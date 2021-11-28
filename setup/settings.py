08f67a3cf53a18b38cb290dc5cedb8e4d87f6a3a
Felipe Oliveira <felipeoes@usp.br>
2021-11-28 17:31:07 -0300

    fazendo deploy da app

--- setup/settings.py
+++ setup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0',
-                 'https://farma-usp.herokuapp.com',
+                 'farma-usp.herokuapp.com',
                  '127.0.0.1']
 
 
